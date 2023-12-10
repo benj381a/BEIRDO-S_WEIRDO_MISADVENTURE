21799295f51155066431a2e2b085d0f006e3425a
spock685 <gustavs@slipgate-ironworks.com>
2023-12-10 03:00:30 +0100

    Comitting portal + main menu script
    Comitting portal + main menu script

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
---
+++ Scripts/Portal.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] int levelID = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadSceneAsync(levelID);
+    }
+}
